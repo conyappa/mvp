0f0a805f3176d8c87eec3085863c108013646bd6
Ariel Martínez Silberstein <amartinez5@uc.cl>
2020-12-09 23:20:50 -0300

    Update the user model

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,19 +1,40 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
 from phonenumber_field.modelfields import PhoneNumberField
-from .managers import UserManager
 from app.base import BaseModel
+
+
+class UserManager(BaseUserManager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+    def everything(self):
+        super().get_queryset()
+
+    def create(self, **fields):
+        user = super().create(**fields)
+        user.set_password(fields["password"])
+        user.save()
+
+    def create_superuser(self, **fields):
+        fields.setdefault("is_staff", True)
+        fields.setdefault("is_superuser", True)
+        return self.create(**fields)
 
 
 class User(BaseModel, AbstractUser):
     USERNAME_FIELD = "phone"
-    REQUIRED_FIELDS = []
 
-    password = None
+    username = None
 
     balance = models.PositiveIntegerField(default=0, verbose_name="account balance")
-    phone = PhoneNumberField(unique=True)
+    phone = PhoneNumberField(
+        unique=True,
+        error_messages={"unique": "A user with that phone number already exists."},
+        verbose_name="phone number",
+    )
 
     objects = UserManager()
 
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
+    path("admin", admin.site.urls),
     path("exception", views.trigger_exception),
     path("bot", include("bot.urls")),
 ]
