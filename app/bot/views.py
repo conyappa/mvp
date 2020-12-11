79d2639a9bde6d877b7e16cc354a4ce303811536
Ariel Martínez Silberstein <amartinez5@uc.cl>
2020-12-11 19:19:48 -0300

    Add the 'text/xml' content-type header to the response

--- app/bot/views.py
+++ app/bot/views.py
@@ -14,10 +14,10 @@ logger = logging.getLogger(__name__)
 class Bot(generics.GenericAPIView):
     def post(self, request):
         incoming_text = request.body.decode()
-        logger.error(incoming_text)
+        # logger.error(request.values)
         handler = getattr(handlers, incoming_text.lower(), lambda: "Lo siento, no sé a qué te refieres.")
         outgoing_text = handler()
         response = MessagingResponse()
         response_msg = response.message()
         response_msg.body(outgoing_text)
-        return HttpResponse(content=response)
+        return HttpResponse(content=response, content_type="text/xml")
