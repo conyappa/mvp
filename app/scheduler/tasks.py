1bed068f8412a1b6f6b285e3c399288eeab6ab6c
Ariel Martínez Silberstein <amartinez5@uc.cl>
2020-12-22 21:52:34 -0300

    Fix typo

--- app/scheduler/tasks.py
+++ app/scheduler/tasks.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.conf import settings
 from lottery.models import Draw
 from accounts.models import User
-from bot.multisender import MultiSenderClient
+from bot import multisender
 from .helpers import use_scheduler
 
 
@@ -16,7 +16,7 @@ def create_new_draw(timestamp):
     draw.create_tickets()
     draw.choose_result()
     # Send a notification.
-    MultiSenderClient().send(
+    multisender.send(
         users=User.objects.all(),
         msg_body_formatter=lambda _user: (
             "¡Ha comenzado un nuevo sorteo! "
@@ -34,7 +34,7 @@ def end_current_draw():
     draw.choose_result()
     draw.conclude()
     # Send a notification.
-    MultiSenderClient().send(
+    multisender.send(
         users=User.objects.all(),
         msg_body_formatter=lambda user: (
             "¡Finalizó el sorteo! Los resultados fueron:\n\n"
@@ -51,7 +51,7 @@ def choose_number_from_current_draw():
     draw = Draw.objects.current()
     draw.choose_result()
     # Broadcast a notification.
-    MultiSenderClient().send(
+    multisender.send(
         users=User.objects.all(),
         msg_body_formatter=lambda _user: (
             "¡Llegó la hora de sacar un número!\n"
