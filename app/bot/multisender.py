d955ed22ba762f11422afb24d59130cacfed3a14
Ariel Martínez Silberstein <amartinez5@uc.cl>
2020-12-22 21:10:39 -0300

    Remove unused code

--- app/bot/multisender.py
+++ app/bot/multisender.py
@@ -9,9 +9,6 @@ class MultiSenderClient(BaseSingletone):
         self.twilio_client = twilio_sender.SenderClient()
 
     def send(self, users, msg_body_formatter, telegram=False, telegram_kwargs={}, twilio=False, twilio_kwargs={}):
-        telegram_users = users.filter(telegram_id__isnull=False)
-        twilio_users = users.filter(phone__isnull=False)
-
         if telegram:
             telegram_users = users.filter(telegram_id__isnull=False)
             self.telegram_client.send(telegram_users, msg_body_formatter, **telegram_kwargs)
