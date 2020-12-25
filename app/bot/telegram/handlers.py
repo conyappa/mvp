1b5e1c14efa7b44e5b4852c63f15b8a66447a5f7
Ariel Martínez Silberstein <amartinez5@uc.cl>
2020-12-25 14:54:19 -0300

    Update username automatically

--- app/bot/telegram/handlers.py
+++ app/bot/telegram/handlers.py
@@ -5,18 +5,16 @@ def start(update, context):
     telegram_user = update.message.from_user
     username = telegram_user.username or str(telegram_user.id)
 
-    user, _created = User.objects.get_or_create(
-        defaults={"username": username, "password": username},
-        telegram_id=telegram_user.id,
-    )
+    user, _created = User.objects.get_or_create(telegram_id=telegram_user.id, defaults={"username": username})
 
-    user.first_name = telegram_user.first_name or ""
-    user.last_name = telegram_user.last_name or ""
+    user.username = username or user.username
+    user.first_name = telegram_user.first_name or user.first_name
+    user.last_name = telegram_user.last_name or user.last_name
     user.save()
 
     greeting_msg = (
-        "¡Bienvenido a ConYappa, una lotería que te premia por ahorrar! 💰💰\n\n"
+        "¡Bienvenido a *ConYappa*, una lotería que te premia por ahorrar! 💰💰\n\n"
         "Mi nombre es YappaBot y seré tu asistente personal. "
         "Envía /reglas y te explicaré cómo participar."
     )
-    update.message.reply_text(greeting_msg)
+    update.message.reply_markdown(greeting_msg)
--- app/bot/telegram/replier.py
+++ app/bot/telegram/replier.py
@@ -19,9 +19,11 @@ def telegram_adapter(handler):
         except User.DoesNotExist:
             update.message.reply_text("¡No nos hemos presentado! Envía /start para comenzar.")
         else:
-            user.first_name = telegram_user.first_name or ""
-            user.last_name = telegram_user.last_name or ""
+            user.username = telegram_user.username or user.username
+            user.first_name = telegram_user.first_name or user.first_name
+            user.last_name = telegram_user.last_name or user.last_name
             user.save()
+
             msg = handler(user)
             update.message.reply_markdown(msg)
 
