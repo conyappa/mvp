80e5117dc2bed5f86d37db13b5f47a1bcbb38509
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-01-26 18:18:09 -0300

    Add the landing page

--- app/app/views.py
+++ app/app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 
 def trigger_exception(_request):
     raise Exception()
 
 
-def landing_page(_request):
-    return redirect("https://www.facebook.com/conyappa/")
+def landing_page(request):
+    # return redirect("https://www.facebook.com/conyappa/")
+    return render(request, "landing_page.html")
