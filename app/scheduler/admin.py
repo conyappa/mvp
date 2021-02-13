fc49d6f71fd847447805b7a63fde4faf229b9423
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-02-13 19:20:46 -0300

    Add 'relevant' status filter for messages

--- app/scheduler/admin.py
+++ app/scheduler/admin.py
@@ -1,6 +1,9 @@
 import logging
+import datetime as dt
 from django import forms
 from django.contrib import admin
+from django.utils import timezone
+from django.db.models import Q
 from .models import Message
 
 
@@ -23,11 +26,41 @@ class MessageForm(forms.ModelForm):
         return super().save(commit=commit)
 
 
+class MessageStatusFieldListFilter(admin.SimpleListFilter):
+    title = "status"
+    parameter_name = "status"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("relevant", "Relevant"),
+            ("not sent", "Not sent"),
+            ("sent", "Sent"),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        qs = queryset
+
+        if value == "relevant":
+            now = timezone.localtime()
+            yesterday = now - dt.timedelta(days=1)
+            query = Q(scheduled_for__gte=yesterday) | (~Q(status=Message.Status.SENT))
+            qs = qs.filter(query)
+
+        elif value == "not sent":
+            qs = qs.exclude(status=Message.Status.SENT)
+
+        elif value == "sent":
+            qs = qs.filter(status=Message.Status.SENT)
+
+        return qs
+
+
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     form = MessageForm
     list_display = ("scheduled_for", "preview", "status", "job_id")
-    list_filter = ("scheduled_for", "status")
+    list_filter = ("scheduled_for", MessageStatusFieldListFilter)
     search_fields = ("text",)
 
     def has_change_permission(self, request, obj=None):
@@ -40,3 +73,16 @@ class MessageAdmin(admin.ModelAdmin):
         max_lenght = 100
         suffix = "…" if (len(obj.text) > max_lenght) else ""
         return obj.text[:max_lenght] + suffix
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        logger.info(request.GET)
+        if not request.GET:
+            now = timezone.localtime()
+            yesterday = now - dt.timedelta(days=1)
+
+            query = Q(scheduled_for__gte=yesterday) | (~Q(status=Message.Status.SENT))
+            qs = qs.filter(query)
+
+        return qs
