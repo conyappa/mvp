c2fb4b6d2b8026dffa0e38521898613283d25108
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-02-23 19:37:56 -0300

    Implement basic GraphQL query and mutation

--- app/accounts/graphql.py
+++ app/accounts/graphql.py
@@ -14,6 +14,18 @@ class EmailListEntryType(DjangoObjectType):
         fields = ("email",)
 
 
+###########
+# QUERIES #
+###########
+
+
+class EmailListEntryQuery(graphene.ObjectType):
+    email_list_entries = graphene.List(EmailListEntryType)
+
+    def resolve_email_list_entries(self, info):
+        return EmailListEntry.objects.all()
+
+
 #############
 # MUTATIONS #
 #############
@@ -29,3 +41,7 @@ class EmailListEntryCreation(graphene.Mutation):
     def mutate(cls, root, info, email):
         entry = EmailListEntry.objects.create(email=email)
         return EmailListEntryCreation(email_list_entry=entry)
+
+
+class EmailListEntryMutation(graphene.ObjectType):
+    create_email_list_entry = EmailListEntryCreation.Field()
--- app/app/graphql.py
+++ app/app/graphql.py
@@ -1,20 +1,17 @@
 import graphene
 from logging import getLogger
-from accounts.graphql import EmailListEntryType, EmailListEntryCreation
+from accounts.graphql import EmailListEntryQuery, EmailListEntryMutation
 
 
 logger = getLogger(__name__)
 
 
-class Query(graphene.ObjectType):
-    all_email_list_entries = graphene.List(EmailListEntryType)
-
-    def resolve_all_email_list_entries(self, info):
-        return EmailListEntryType._meta.model.objects.all()
+class Query(EmailListEntryQuery, graphene.ObjectType):
+    pass
 
 
-class Mutation(graphene.ObjectType):
-    create_email_list_entry = EmailListEntryCreation.Field()
+class Mutation(EmailListEntryMutation, graphene.ObjectType):
+    pass
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
